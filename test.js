86998d854c369084c29646443e0f8fb9cecdf398
Dishant Yadav <dishantyadav84@gmail.com>
2024-09-24 20:59:15 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.button');
+const body =  document.querySelector('body');
+
+
+
+
+function purple(){
+  body.style.backgroundColor = "#987D9A";
+  body.style.color = "black";
+
+}
+function cyan(){
+  body.style.backgroundColor = "#91DDCF";
+}
+function blue(){
+  body.style.backgroundColor = "#5BBCFF";
+  body.style.color = "black";
+}
+function green(){
+  body.style.backgroundColor = "#00712D";
+  body.style.color = "white";
+  
+}
+function white(){
+  body.style.backgroundColor = "white";
+  body.style.color = "black";
+}
